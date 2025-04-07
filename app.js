d3b34bbb8ce3e386bd5fa1e9dc822c2d0cdbf568
Priyanshu <ribupkashyap@gmail.com>
2025-04-07 18:44:45 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+let input = document.querySelector("input");
+let addTask = document.querySelector("#addTask");
+let ul = document.querySelector("ul");
+
+addTask.addEventListener("click", function(){
+    let item = document.createElement("li");
+    item.innerText = input.value;
+    ul.appendChild(item);
+    input.value = "";
+
+    let dltbtn = document.createElement("button");
+    dltbtn.classList.add("delete");
+    dltbtn.innerText = "Delete";
+    item.appendChild(dltbtn);
+});
+
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName == "BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+        console.log("Deleted");
+    }
+
+});
+
+// let dltbtns = document.querySelectorAll(".delete");
+
+// for(dltbtn of dltbtns){
+//     dltbtn.addEventListener("click", function(){
+//         let par = this.parentElement;
+//         par.remove();
+//     });
+// };
